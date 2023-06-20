10837358ada35dfce3c90a220987a1516a86f37e
Ruid-AI <127470787+Ruid-AI@users.noreply.github.com>
2023-06-20 15:45:45 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Aietas AI时代",
+  description: "一起享受AI时代.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Aietas AI时代",
     statusBarStyle: "default",
   },
 };
